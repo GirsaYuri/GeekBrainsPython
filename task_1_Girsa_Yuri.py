b2a9fceff3fdcf9f6ac9700f5d3f4825d808d59b
Yuri <girsau@mail.ru>
2021-11-08 19:36:29 +0700

    Выполнил задания.

---
+++ task_1_Girsa_Yuri.py
@@ -0,0 +1,20 @@
+def num_translate(number):
+    for key, val in translate.items():
+        if number == key:
+            return val
+
+
+translate = {
+    'one': 'один',
+    'two': 'два',
+    'three': 'три',
+    'four': 'четыре',
+    'five': 'пять',
+    'six': 'шесть',
+    'seven': 'семь',
+    'eight': 'восемь',
+    'nine': 'девять',
+    'ten': 'десять',
+}
+
+print(num_translate('two'))
