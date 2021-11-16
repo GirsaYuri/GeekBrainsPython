d739d4ad7df1db41ad183545027936f74da1606f
Yuri <girsau@mail.ru>
2021-11-16 21:43:48 +0700

    Сделал 5 урок

---
+++ task_2_Girsa_Yuri.py
@@ -0,0 +1,13 @@
+n = 15
+nums = (num for num in range(1, n + 1, 2))
+
+gen_nums = nums
+
+print(next(gen_nums))
+print(next(gen_nums))
+print(next(gen_nums))
+print(next(gen_nums))
+print(next(gen_nums))
+print(next(gen_nums))
+print(next(gen_nums))
+print(next(gen_nums))
